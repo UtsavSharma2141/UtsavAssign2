ade2cd33718f06117065d745bd179b2cdc07daa7
UtsavSharma2141 <n01392141@humbermail.ca>
2021-02-19 17:32:06 -0500

    Initial commit.

---
+++ UtsavAssign22/src/main/java/utsav/sharma/n01392141/UtsavActivity.java
@@ -0,0 +1,15 @@
+/* Utsav Sharma, N01392141 Section C */
+package utsav.sharma.n01392141;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UtsavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
