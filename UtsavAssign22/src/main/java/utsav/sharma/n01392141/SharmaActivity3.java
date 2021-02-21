2e4d3b820fa4bb1043a39f8ee10542ed84932842
UtsavSharma2141 <n01392141@humbermail.ca>
2021-02-20 20:00:29 -0500

    created and worked on SharmaActivity4.java and activity_sharma4.xml files. done SharmaActivity3.java file.

--- UtsavAssign22/src/main/java/utsav/sharma/n01392141/SharmaActivity3.java
+++ UtsavAssign22/src/main/java/utsav/sharma/n01392141/SharmaActivity3.java
@@ -2,11 +2,19 @@ package utsav.sharma.n01392141;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -23,6 +31,10 @@ public class SharmaActivity3 extends AppCompatActivity {
     ArrayList<String> toppArr;
     TextView pizaTypeTxt, pizaSizeTxt, pizaToppTxt;
 
+    Spinner provinceSpinner;
+    Button orderbtn;
+
+    String province;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +57,7 @@ public class SharmaActivity3 extends AppCompatActivity {
 
         pizaTypeTxt.setText(pizaType);
         pizaSizeTxt.setText(pizaSize);
-        pizaToppTxt.setText((CharSequence) toppArr);
+        pizaToppTxt.setText(toppArr.toString());
 
         nameEdt.addTextChangedListener(new TextWatcher() {
             @Override
@@ -79,7 +91,9 @@ public class SharmaActivity3 extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-
+                if(s.length() <= 0){
+                     creditEdt.setError("Enter details");
+                }
             }
         });
 
@@ -96,10 +110,58 @@ public class SharmaActivity3 extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
+                if(s.length() <= 0){
+                    addEdt.setError("Enter details");
+                }
+            }
+        });
+
+        provinceSpinner = findViewById(R.id.utsavProvinceSpin);
+        provinceSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.select_dialog_item,getResources().getStringArray(R.array.province)));
+
+        provinceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                province = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
 
+        orderbtn = findViewById(R.id.utsavOrderBtn);
+        orderbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEdt.getText().toString();
+                String creditNo = creditEdt.getText().toString();
+                String addr = addEdt.getText().toString();
+                
 
+                name = name.replaceAll("\\d","");
+
+                if(name.length() < 3 || creditNo.length() < 16 || addr.length() == 0){
+                    String msg = getResources().getString(R.string.toast3);
+                    Toast.makeText(SharmaActivity3.this, msg, Toast.LENGTH_SHORT).show();
+                }else{
+                    // String msg = getResources().getString(R.string.toast3);
+
+                   // Toast.makeText(SharmaActivity3.this, msg, Toast.LENGTH_SHORT).show();
+                    Intent ii = new Intent(SharmaActivity3.this,SharmaActivity4.class);
+                    ii.putExtra("type",pizaTypeTxt.getText().toString());
+                    ii.putExtra("size",pizaSizeTxt.getText().toString());
+                    ii.putExtra("topp",pizaToppTxt.getText().toString());
+
+                    ii.putExtra("uname",name);
+                    ii.putExtra("ucredit",creditNo);
+                    ii.putExtra("uaddr",addr);
+                    ii.putExtra("uprov",province);
+                    startActivity(ii);
+
+                }
+            }
+        });
     }
 }
---
+++ UtsavAssign22/src/main/java/utsav/sharma/n01392141/SharmaActivity4.java
@@ -0,0 +1,49 @@
+package utsav.sharma.n01392141;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SharmaActivity4 extends AppCompatActivity {
+
+    TextView typetxt, sizetxt, topptxt, nametxt, addtxt, creditxt, provincetxt;
+    Button checkBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sharma4);
+
+        typetxt = findViewById(R.id.utsavPType);
+        sizetxt = findViewById(R.id.utsavPSize);
+        topptxt = findViewById(R.id.utsavPTopp);
+
+        nametxt = findViewById(R.id.utsavUname);
+        addtxt = findViewById(R.id.utsavUadd);
+        creditxt = findViewById(R.id.utsavUcredit);
+        provincetxt = findViewById(R.id.utsavUProvince);
+
+
+        typetxt.setText(getIntent().getStringExtra("type"));
+        sizetxt.setText(getIntent().getStringExtra("size"));
+        topptxt.setText(getIntent().getStringExtra("topp"));
+
+        nametxt.setText(getIntent().getStringExtra("uname"));
+        addtxt.setText(getIntent().getStringExtra("uaddr"));
+        creditxt.setText(getIntent().getStringExtra("ucredit"));
+        provincetxt.setText(getIntent().getStringExtra("uprov"));
+
+        checkBtn = findViewById(R.id.utsavCheckTBtn);
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SharmaActivity4.this);
+            }
+        });
+
+    }
+}
